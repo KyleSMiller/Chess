165042726bf4c9aa7f229cb2a7bd5bb7688b6d61
Kyle Miller <kylesmiller38@gmail.com>
2019-11-20 11:12:46 -0700

    added King piece and modified some ChessPiece methods

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,25 +1,37 @@
 public abstract class ChessPiece {
 
     private String image;
+    private int[] position;
+    private Color color;
+
     private int forward;
     private int backwards;
-    private int left;
-    private int right;
+    private int side;
     private int diagonalUp;
     private int diagonalDown;
 
-    public ChessPiece(String image, int forward, int backwards, int left, int right, int diagonalUp, int diagonalDown){
+    public ChessPiece(String image, int[] position, Color color, int forward, int backwards, int side, int diagonalUp, int diagonalDown){
         this.image = image;
+        this.position = position;  // [column, row]
+        this.color = color;
+
         this.forward = forward;
         this.backwards = backwards;
-        this.left = left;
-        this.right = right;
+        this.side = side;
         this.diagonalUp = diagonalUp;
         this.diagonalDown = diagonalDown;
     }
 
     public String getImage(){
         return image;
+    }
+
+    public int[] getPosition(){
+        return position;
+    }
+
+    public Color color(){
+        return color;
     }
 
     public int getForward() {
@@ -30,12 +42,8 @@ public abstract class ChessPiece {
         return backwards;
     }
 
-    public int getLeft() {
-        return left;
-    }
-
-    public int getRight() {
-        return right;
+    public int getSide() {
+        return side;
     }
 
     public int getDiagonalUp() {
@@ -46,15 +54,23 @@ public abstract class ChessPiece {
         return diagonalDown;
     }
 
-    public abstract int getMoves();
+    public abstract int[][] getMoves();
 
-    public abstract int getAttacks();
+    public abstract int[][] getAttacks();
+
+    public void setPosition(int[] newPosition){
+        this.position = newPosition;
+    }
 
     /**
      * Fade the piece away
      */
     public void fadeOut(){
 
+    }
+
+    public enum Color{
+        WHITE, BLACK
     }
 
 }
---
+++ src/King.java
@@ -0,0 +1,41 @@
+public class King extends ChessPiece{
+
+    public King(int[] position, Color color){
+        super("temp", position, color, 1, 1, 1, 1, 1);
+    }
+
+    public int[][] getMoves(){
+        int[][] moves = new int[8][2];  // [up, down, left, right, leftUp, rightUp, leftDown, rightDown][column, row]
+        // up
+        moves[0][0] = getPosition()[0];
+        moves[0][1] = getPosition()[1] - getForward();
+        // down
+        moves[1][0] = getPosition()[0];
+        moves[1][1] = getPosition()[1] + getForward();
+        // left
+        moves[2][0] = getPosition()[0] - getSide();
+        moves[2][1] = getPosition()[1];
+        // right
+        moves[3][0] = getPosition()[0] + getSide();
+        moves[3][1] = getPosition()[1];
+        // leftUp
+        moves[4][0] = getPosition()[0] - getDiagonalUp();
+        moves[4][1] = getPosition()[1] - getDiagonalUp();
+        // rightUp
+        moves[5][0] = getPosition()[0] + getDiagonalUp();
+        moves[5][1] = getPosition()[1] - getDiagonalUp();
+        // leftDown
+        moves[6][0] = getPosition()[0] - getDiagonalDown();
+        moves[6][1] = getPosition()[1] + getDiagonalDown();
+        // rightDown
+        moves[7][0] = getPosition()[0] + getDiagonalDown();
+        moves[7][1] = getPosition()[1] + getDiagonalDown();
+
+        return moves;
+    }
+
+    public int[][] getAttacks(){
+        return this.getMoves();  // King can attack in any direction he can move
+    }
+
+}
